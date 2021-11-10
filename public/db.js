eede9e0aef057a5f4511cd018498fe2acddcf84b
Bryanna Vinge <bevinge711@gmail.com>
2021-11-10 13:27:19 -0600

    db.js written

--- public/db.js
+++ public/db.js
@@ -2,22 +2,19 @@ let db;
 
 const request = indexedDB.open("budgetDB", 1);
 
-request.onupgradeneeded = () => {
-
+request.onupgradeneeded = (event) => {
+    db = event.target.result;
+    db.createObjectStore("budgetStore", { autoIncrement: true })
 };
 
-request.onsuccess = () => {
-
-};
-
-request.onerror = () => {
-
+request.onerror = (event) => {
+    console.log(`error : ${event.target.errorCode}`)
 };
 
 const checkDatabase = () => {
-    let transaction = db.transaction([""], "readwrite");
+    let transaction = db.transaction(["budgetStore"], "readwrite");
 
-    const store = transaction.objectStore("");
+    const store = transaction.objectStore("budgetStore");
 
     const getAll = store.getAll();
 
@@ -44,3 +41,21 @@ const checkDatabase = () => {
         }
     };
 }
+request.onsuccess = (event) => {
+    console.log("Successful");
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        console.log("Backend is Online");
+        checkDatabase();
+    }
+};
+
+const saveRec = (record) => {
+    console.log("Saving Record");
+    const transaction = db.transaction(["budgetStore"], "readwrite");
+    const store = transaction.objectStore("budgetStore");
+    store.add(record);
+};
+
+window.addEventListener("online", checkDatabase)
